34c7e035be3de918decd9b48702c448d8a46287a
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2023-05-18 17:38:02 +0900

    IllustCellViewの追加

---
+++ Pixiv-ios-training/IllustCell.swift
@@ -0,0 +1,16 @@
+//
+//  IllustCell.swift
+//  Pixiv-ios-training
+//
+//  Created by 上條栞汰 on 2023/05/18.
+//
+
+import UIKit
+
+final class IllustCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = .red
+    }
+}
