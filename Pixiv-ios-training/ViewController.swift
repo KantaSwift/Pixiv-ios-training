f63c7b6214da3d9c102f16e25e8185177e93cb00
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2023-05-18 15:54:02 +0900

    first commit

---
+++ Pixiv-ios-training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pixiv-ios-training
+//
+//  Created by 上條栞汰 on 2023/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
